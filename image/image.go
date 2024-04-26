366af0e79a352dd6290f6d84cb3f7c4f516cc2c2
stuartdd <stuartdd@gmail.com>
2024-04-26 20:40:31 +0100

    refine image

--- image/image.go
+++ image/image.go
@@ -6,22 +6,29 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type image struct {
-	name    string
-	walker  *walker
-	exifPos int
-	iiPos   int
-	offset  uint32
+	name       string
+	walker     *walker
+	sof        string
+	exif       bool
+	littleE    bool
+	app1Marker string
+	app1Size   uint32 // APP1 data size
 }
 
 func (p *image) IsBigE() bool {
-	return p.iiPos < 4
+	return !p.littleE
+}
+
+func (p *image) IsExif() bool {
+	return p.exif
 }
 
 func (p *image) String() string {
-	return fmt.Sprintf("IMG: EXIF[%t] LittleE[%t] Offset[%d] FileLen[%d] Name[%s]", p.exifPos > 0, p.iiPos > 0, p.offset, p.walker.len, p.name)
+	return fmt.Sprintf("IMG: SOF[%s]  APP1 Size[%d] EXIF[%t] LittleE[%t] FileLen[%d] Name[%s]", p.sof, p.app1Size, p.IsExif(), p.IsBigE(), p.walker.len, p.name)
 }
 
 type walker struct {
@@ -38,6 +45,25 @@ func NewWalker(b *[]uint8, len int) *walker {
 		len:  len,
 		bigE: false,
 	}
+}
+
+func (p *walker) Zstring(max int) string {
+	var line bytes.Buffer
+	count := 0
+	for p.canAdvance() {
+		b := p.Advance(1)
+		if b > 0 && count < max {
+			line.WriteByte(byte(b))
+		} else {
+			return line.String()
+		}
+		count++
+	}
+	return ""
+}
+
+func (p *walker) ZstringEquals(s string) bool {
+	return (strings.ToLower(p.Zstring(len(s)+2)) == strings.ToLower(s))
 }
 
 func (p *walker) Hex8() string {
@@ -109,6 +135,10 @@ func (p *walker) Int16BE() uint32 {
 
 func (p *walker) canNotAdvance() bool {
 	return p.pos >= p.len
+}
+
+func (p *walker) canAdvance() bool {
+	return p.pos < p.len
 }
 
 func (p *walker) Reset() *walker {
@@ -245,13 +275,54 @@ func GetImage(path string) (*image, error) {
 	}
 	walker := NewWalker(&byteArray, len(byteArray))
 	image := &image{
-		name:    path,
-		walker:  walker,
-		exifPos: walker.SearchFromStart("Exif"),
-		iiPos:   walker.SearchFromStart("II*"),
-		offset:  walker.Pos(4).Int16BE() - 2,
+		name:       path,
+		walker:     walker,
+		sof:        walker.Pos(0).Hex16(),
+		app1Marker: walker.Hex16(),
+		app1Size:   walker.Int32BE() - 2,
+		exif:       walker.Pos(6).ZstringEquals("Exif"),
+		littleE:    walker.Pos(12).ZstringEquals("II*"),
 	}
 	walker.SetBigE(image.IsBigE())
 	walker.Reset()
 	return image, nil
 }
+
+/*
+254	00FE	NewSubfileType	A general indication of the kind of data contained in this subfile.
+255	00FF	SubfileType	A general indication of the kind of data contained in this subfile.
+256	0100	ImageWidth	The number of columns in the image, i.e., the number of pixels per row.
+257	0101	ImageLength	The number of rows of pixels in the image.
+258	0102	BitsPerSample	Number of bits per component.
+259	0103	Compression	Compression scheme used on the image data.
+262	0106	PhotometricInterpretation	The color space of the image data.
+263 0107 	Threshholding 	For black and white TIFF files that represent shades of gray, the technique used to convert from gray to black and white pixels.
+264 0108 	CellWidth 	The width of the dithering or halftoning matrix used to create a dithered or halftoned bilevel file.
+265 0109 	CellLength 	The length of the dithering or halftoning matrix used to create a dithered or halftoned bilevel file.
+266	010A	FillOrder	The logical order of bits within a byte.
+270	010E	ImageDescription	A string that describes the subject of the image.
+271	010F	Make	The scanner manufacturer.
+272	0110	Model	The scanner model name or number.
+273	0111	StripOffsets	For each strip, the byte offset of that strip.
+274	0112	Orientation	The orientation of the image with respect to the rows and columns.
+277	0115	SamplesPerPixel	The number of components per pixel.
+278	0116	RowsPerStrip	The number of rows per strip.
+279	0117	StripByteCounts	For each strip, the number of bytes in the strip after compression.
+280	0118	MinSampleValue	The minimum component value used.
+281	0119	MaxSampleValue	The maximum component value used.
+282	011A	XResolution	The number of pixels per ResolutionUnit in the ImageWidth direction.
+283	011B	YResolution	The number of pixels per ResolutionUnit in the ImageLength direction.
+284	011C	PlanarConfiguration	How the components of each pixel are stored.
+288	0120	FreeOffsets	For each string of contiguous unused bytes in a TIFF file, the byte offset of the string.
+289 0121 	FreeByteCounts 	For each string of contiguous unused bytes in a TIFF file, the number of bytes in the string.
+290	0122	GrayResponseUnit	The precision of the information contained in the GrayResponseCurve.
+291	0123	GrayResponseCurve	For grayscale data, the optical density of each possible pixel value.
+296	0128	ResolutionUnit	The unit of measurement for XResolution and YResolution.
+305	0131	Software	Name and version number of the software package(s) used to create the image.
+306	0132	DateTime	Date and time of image creation.
+315	013B	Artist	Person who created the image.
+316	013C	HostComputer	The computer and/or operating system in use at the time of image creation.
+320	0140	ColorMap	A color map for palette color images.
+338	0152	ExtraSamples	Description of extra components.
+33432	8298	Copyright	Copyright notice.
+*/
