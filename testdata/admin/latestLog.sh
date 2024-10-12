b0947fa7ff463948785e9f0024af92641b206d07
stuartdd <stuartdd@gmail.com>
2024-10-12 14:28:48 +0100

    logging request

--- testdata/admin/latestLog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path=$(pwd)/logs
-
-file=$(ls $path -tp | grep -v /$ | head -1)
-
-echo "Latest Log file: $file"
-echo "-------------------------------------------------------------------------------------------------"
-cat $path/$file
